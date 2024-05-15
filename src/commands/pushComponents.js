579947970ae082c813d889879e0ec00bb2c9e451
Fearne York <fearne.york@cinch.co.uk>
2024-05-15 10:53:07 +0100

    feat(main): add rest of push component flow in

--- src/commands/pushComponents.js
+++ src/commands/pushComponents.js
@@ -1,23 +1,20 @@
 import inquirer from "inquirer";
 import subProcess from "child_process";
+import ora from "ora";
 
 export default async function pushComponents() {
-  let componentDir;
+  const tempDir = "~/Desktop/temp_storyblok_cli";
   const dirsArr = [];
+  const componentArr = [];
 
-  subProcess.exec(
-    `ls ~/Desktop/temp_storyblok_cli`,
-    (error, stdout, stderr) => {
-      const isDirAvailable = !stderr.includes("No such file or directory");
-      if (stderr && !isDirAvailable) {
-        console.error(
-          "No such file or directory. Please pull components first.",
-        );
-        process.exit(1);
-      }
-      dirsArr.push(...stdout.toString().split("\n").slice(0, -1));
-    },
-  );
+  subProcess.exec(`ls ${tempDir}`, (error, stdout, stderr) => {
+    const isDirAvailable = !stderr.includes("No such file or directory");
+    if (stderr && !isDirAvailable) {
+      console.error("No such file or directory. Please pull components first.");
+      process.exit(1);
+    }
+    dirsArr.push(...stdout.toString().split("\n").slice(0, -1));
+  });
 
   setTimeout(() => {
     inquirer
@@ -30,8 +27,64 @@ export default async function pushComponents() {
         },
       ])
       .then(({ whichDir }) => {
-        //TODO: create inquirer-search-list from selected directory
-        console.log(whichDir);
+        subProcess.exec(
+          `cd ${tempDir}/${whichDir} && ls -a`,
+          (error, stdout, stderr) => {
+            if (error) {
+              console.error(error);
+              process.exit(1);
+            }
+            componentArr.push(...stdout.toString().split("\n").slice(2));
+          },
+        );
+        setTimeout(() => {
+          inquirer
+            .prompt([
+              {
+                type: "checkbox",
+                name: "whichComponents",
+                message: "Which components would you like to push?",
+                choices: componentArr,
+              },
+            ])
+            .then(({ whichComponents }) => {
+              if (whichComponents.length === 0) {
+                console.error(
+                  "No components selected. Please select at least one component to push.",
+                );
+                process.exit(1);
+              }
+              setTimeout(() => {
+                inquirer
+                  .prompt([
+                    {
+                      type: "list",
+                      name: "whichSpace",
+                      message:
+                        "Which space would you like to pull components from?",
+                      choices: [
+                        { name: "dev", value: "132543" },
+                        { name: "prod", value: "143588" },
+                      ],
+                    },
+                  ])
+                  .then(({ whichSpace }) => {
+                    subProcess.exec(
+                      `cd ${tempDir}/${whichDir} && storyblok push-components ${whichComponents.join(",")} --space=${whichSpace}`,
+                      (error) => {
+                        const spinner = ora("Pushing components...").start();
+                        spinner.spinner = "clock";
+                        if (error) {
+                          console.error(error);
+                          process.exit(1);
+                        }
+                        spinner.succeed("Components pushed successfully!");
+                      },
+                    );
+                  });
+              }, 1500);
+            });
+        }, 1500);
       });
   }, 1000);
 }
